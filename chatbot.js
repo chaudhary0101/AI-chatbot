7ddbe62a7cf044d8528a00c40ae6120ddeaf9f68
chaudhary0101 <vansh.chaudhary4456@gmail.com>
2025-08-19 23:11:30 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -35,6 +35,20 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+const cors = require('cors');
+const allowedOrigins = ['https://your-chatbot.netlify.app'];
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+
 
 
 
